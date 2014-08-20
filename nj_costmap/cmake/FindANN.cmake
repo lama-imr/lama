f115b099aba5635a07460b2e2aeefed5b72612a2
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-08-20 13:28:08 +0000

    nj_costmap: Use system library ann
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@128 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

--- nj_costmap/cmake/FindANN.cmake
+++ nj_costmap/cmake/FindANN.cmake
@@ -1,21 +1,44 @@
-# - Find ANN
-# Find the native ANN includes and library
-# This module defines
-#  ANN_INCLUDE_DIR, where to find ANN/ANN.h, etc.
-#  ANN_LIBRARIES, the libraries needed to use ANN.
-#  ANN_FOUND, If false, do not try to use ANN.
-# also defined, but not for general use are
-#  ANN_LIBRARY, where to find the ANN library.
+# -*- mode: cmake -*-
+#
+#  This file is part of the Feel++ library
+#
+#  Author(s): Christophe Prud'homme <christophe.prudhomme@ujf-grenoble.fr>
+#       Date: 2010-02-10
+#
+#  Copyright (C) 2010 Université Joseph Fourier
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# this files defines
+#  - ANN_INCLUDE_DIR
+#  - ANN_LIBRARIES
+#  - ANN_FOUND
+
+INCLUDE(CheckIncludeFileCXX)
+# CHECK_INCLUDE_FILE_CXX(ANN/ANN.h FEELPP_HAS_ANN_H)
+
+
+FIND_LIBRARY( ANN_LIB ann ANN PATHS /usr/lib /opt/local/lib  $ENV{ANN_DIR}/lib)
+SET(ANN_LIBRARIES ${ANN_LIB} )
+
+FIND_PATH(ANN_INCLUDE_DIR
+  ANN.h
+  PATHS /usr/include/ /usr/include/ANN /opt/local/include/ANN /usr/local/include/ANN  $ENV{ANN_DIR}/include/ANN
+  DOC "Directory where ANN header files are stored" )
 
 include(FindPackageHandleStandardArgs)
-
-find_path(ANN_INCLUDE_DIR ANN/ANN.h)
-find_library(ANN_LIBRARY ANN)
-
-find_package_handle_standard_args(ANN DEFAULT_MSG
-    ANN_LIBRARY ANN_INCLUDE_DIR)
-
-if(ANN_FOUND)
-    set(ANN_INCLUDE_DIRS ${ANN_INCLUDE_DIR})
-    set(ANN_LIBRARIES ${ANN_LIBRARY})
-endif()
+find_package_handle_standard_args(ANN "Could not find ANN " ANN_INCLUDE_DIR ANN_LIBRARIES)
+# show the BERKELEY_DB_INCLUDE_DIR and BERKELEY_DB_LIBRARIES variables only in the advanced view
+MARK_AS_ADVANCED(ANN_INCLUDE_DIR ANN_LIBRARIES )
