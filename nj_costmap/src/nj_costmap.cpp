c358cd1969feff44e637b73e014393ae5f562c5a
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-08-21 12:36:18 +0000

    nj_costmap: code refactoring
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@132 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

--- nj_costmap/src/nj_costmap.cpp
+++ nj_costmap/src/nj_costmap.cpp
@@ -52,11 +52,11 @@ std::string odom_frame;
  */
 double angleFromQuaternion(const tf::Quaternion q)
 {
-	if (std::fabs(q.x()) > 1e-5 || std::fabs(q.y()) > 1e-5)
-	{
-		ROS_WARN("Laser frame rotation is not around the z-axis, just pretending it is");
-	}
-	return 2 * std::atan2(q.z(), q.w());
+  if (std::fabs(q.x()) > 1e-5 || std::fabs(q.y()) > 1e-5)
+  {
+    ROS_WARN("Laser frame rotation is not around the z-axis, just pretending it is");
+  }
+  return 2 * std::atan2(q.z(), q.w());
 }
 
 /* Return the twist to reach the given goal
@@ -68,149 +68,149 @@ double angleFromQuaternion(const tf::Quaternion q)
  */
 geometry_msgs::Twist goToGoal(const double xgoal, const double ygoal, const nav_msgs::OccupancyGrid& map)
 {
-	geometry_msgs::Twist twist;
-	
-	// Get the rotation between the map and odom_frame.
-	tf::TransformListener tfListener;
-	tf::StampedTransform tr;
-	try
-	{
-		tfListener.waitForTransform(map.header.frame_id, odom_frame,
-				map.header.stamp, ros::Duration(1.0));
-		tfListener.lookupTransform(map.header.frame_id, odom_frame,
-				map.header.stamp, tr);
-	}
-	catch (tf::TransformException ex)
-	{
-		ROS_ERROR("Error in nj_costmap");
-		ROS_ERROR("%s", ex.what());
-	}
-	
-	double distance = std::sqrt(xgoal * xgoal + ygoal * ygoal);
-	if (distance > max_distance)
-	{
-		ROS_DEBUG("distance to goal (%f) is longer than max (%f)", distance, max_distance);
-	}
-	// thetarobot is the rotation of the robot relative to the map.
-	double thetarobot = angleFromQuaternion(tr.getRotation());
-	double thetagoal = std::atan2(ygoal, xgoal); // In map coordinate system
-	double dtheta = std::fmod(thetagoal - thetarobot + M_PI, 2 * M_PI) - M_PI;
-	ROS_DEBUG("thetarobot: %f, thetagoal: %f, dtheta: %f", thetarobot, thetagoal, dtheta);
-	if (dtheta > maxtheta) dtheta = maxtheta;
-	if (dtheta < -maxtheta) dtheta = -maxtheta;
+  geometry_msgs::Twist twist;
 
-	// Only move forward if the goal is in front of the robot (+/- maxtheta)
-	double vx = kp_v * distance * (maxtheta - std::fabs(dtheta)) / maxtheta; 
-	double wz = kp_w * dtheta;
-	if (distance < reach_distance)
-	{
-		vx = 0;
-		wz = 0;
-	}
-	else if (vx < min_velocity)
-	{
-		vx = min_velocity;
-	}
+  // Get the rotation between the map and odom_frame.
+  tf::TransformListener tfListener;
+  tf::StampedTransform tr;
+  try
+  {
+    tfListener.waitForTransform(map.header.frame_id, odom_frame,
+        map.header.stamp, ros::Duration(1.0));
+    tfListener.lookupTransform(map.header.frame_id, odom_frame,
+        map.header.stamp, tr);
+  }
+  catch (tf::TransformException ex)
+  {
+    ROS_ERROR("Error in nj_costmap");
+    ROS_ERROR("%s", ex.what());
+  }
 
-	
-	twist.linear.x = vx;
-	twist.angular.z = wz;
-	return twist;
+  double distance = std::sqrt(xgoal * xgoal + ygoal * ygoal);
+  if (distance > max_distance)
+  {
+    ROS_DEBUG("distance to goal (%f) is longer than max (%f)", distance, max_distance);
+  }
+  // thetarobot is the rotation of the robot relative to the map.
+  double thetarobot = angleFromQuaternion(tr.getRotation());
+  double thetagoal = std::atan2(ygoal, xgoal); // In map coordinate system
+  double dtheta = std::fmod(thetagoal - thetarobot + M_PI, 2 * M_PI) - M_PI;
+  ROS_DEBUG("thetarobot: %f, thetagoal: %f, dtheta: %f", thetarobot, thetagoal, dtheta);
+  if (dtheta > maxtheta) dtheta = maxtheta;
+  if (dtheta < -maxtheta) dtheta = -maxtheta;
+
+  // Only move forward if the goal is in front of the robot (+/- maxtheta)
+  double vx = kp_v * distance * (maxtheta - std::fabs(dtheta)) / maxtheta; 
+  double wz = kp_w * dtheta;
+  if (distance < reach_distance)
+  {
+    vx = 0;
+    wz = 0;
+  }
+  else if (vx < min_velocity)
+  {
+    vx = min_velocity;
+  }
+
+
+  twist.linear.x = vx;
+  twist.angular.z = wz;
+  return twist;
 }
 
 void handleCostmap(nav_msgs::OccupancyGrid msg)
 {
-	loc.crossDetect(msg);
-	std::vector<double> desc = loc.getCrossDescriptor();
+  loc.crossDetect(msg);
+  std::vector<double> desc = loc.getCrossDescriptor();
 
-	ROS_DEBUG("CROSS x: %.3f, y: %.3f, r: %.3f, nroads: %lu", desc[0], desc[1], desc[2], desc.size() - 3);
+  ROS_DEBUG("CROSS x: %.3f, y: %.3f, r: %.3f, nroads: %zu", desc[0], desc[1], desc[2], desc.size() - 3);
 
-	geometry_msgs::Twist twist = goToGoal(desc[0], desc[1], msg);
-	pub_twist.publish(twist);
+  geometry_msgs::Twist twist = goToGoal(desc[0], desc[1], msg);
+  pub_twist.publish(twist);
 
-	// Visualization: a sphere at detected crossing center
-	if (marker.getNumSubscribers())
-	{
-		visualization_msgs::Marker m = getXingMarker(msg.header.frame_id,
-				loc.getCrossCenterX(), loc.getCrossCenterY(), loc.getCrossRadius());
-		marker.publish(m);
-	}
+  // Visualization: a sphere at detected crossing center
+  if (marker.getNumSubscribers())
+  {
+    visualization_msgs::Marker m = getXingMarker(msg.header.frame_id,
+        loc.getCrossCenterX(), loc.getCrossCenterY(), loc.getCrossRadius());
+    marker.publish(m);
+  }
 
-	// Visualization: a line at each detected road
-	if (roadMarker.getNumSubscribers())
-	{
-		std::vector<double> exitAngles = loc.getExitAngles();
-		visualization_msgs::Marker ml = getRoadsMarker(msg.header.frame_id, exitAngles, loc.getCrossRadius());
-		roadMarker.publish(ml);
-	}
+  // Visualization: a line at each detected road
+  if (roadMarker.getNumSubscribers())
+  {
+    std::vector<double> exitAngles = loc.getExitAngles();
+    visualization_msgs::Marker ml = getRoadsMarker(msg.header.frame_id, exitAngles, loc.getCrossRadius());
+    roadMarker.publish(ml);
+  }
 
-	// Fake laser.
-	if (fakeLaserMarker.getNumSubscribers())
-	{
-		sensor_msgs::LaserScan scan = getFakeLaser(msg);
-		fakeLaserMarker.publish(scan);
-	}
+  // Fake laser.
+  if (fakeLaserMarker.getNumSubscribers())
+  {
+    sensor_msgs::LaserScan scan = getFakeLaser(msg);
+    fakeLaserMarker.publish(scan);
+  }
 }
 
 bool executeNavigate(const li::NavigateGoalConstPtr& goal, NavServer* server)
 {
-	li::lmi_laser_descriptor_get ldg;
-	li::NavigateResult result;
+  li::lmi_laser_descriptor_get ldg;
+  li::NavigateResult result;
 
-	switch (goal->action)
-	{
-		case li::NavigateGoal::STOP:
-			ROS_DEBUG("Navigation STOP");
-			result.final_state = result.STOPPED;
-			break;
-		case li::NavigateGoal::TRAVERSE:
-			ROS_DEBUG("Navigation START");
-			ldg.request.id = goal->descriptor;
-			ros::service::call("lmi_laser_descriptor_getter", ldg);
-			// TODO: rewrite this file using the new Navigate.action.
-			break;
-	}
-	return true;
+  switch (goal->action)
+  {
+    case li::NavigateGoal::STOP:
+      ROS_DEBUG("Navigation STOP");
+      result.final_state = result.STOPPED;
+      break;
+    case li::NavigateGoal::TRAVERSE:
+      ROS_DEBUG("Navigation START");
+      ldg.request.id = goal->descriptor;
+      ros::service::call("lmi_laser_descriptor_getter", ldg);
+      // TODO: rewrite this file using the new Navigate.action.
+      break;
+  }
+  return true;
 }
 
 
 int main(int argc, char **argv)
 {
-	// Debug log level
-	if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info))
-	{
-		ros::console::notifyLoggerLevelsChanged();
-	}
+  // Debug log level
+  if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info))
+  {
+    ros::console::notifyLoggerLevelsChanged();
+  }
 
-	ros::init(argc, argv, "costmap_jockey");
-	ros::NodeHandle n("~");
+  ros::init(argc, argv, "costmap_jockey");
+  ros::NodeHandle n("~");
 
-	/* Frame of the laser sensor */
-	n.param<std::string>("odom_frame", odom_frame, "odom");
-	ROS_INFO("odom_frame: %s", odom_frame.c_str());
+  /* Frame of the laser sensor */
+  n.param<std::string>("odom_frame", odom_frame, "odom");
+  ROS_INFO("odom_frame: %s", odom_frame.c_str());
 
-	/* Minimal frontier width */
-	if (!n.hasParam("frontier_width"))
-	{
-		ROS_ERROR("Parameter frontier_width not set, exiting.");
-		return 1;
-	}
-	double frontier_width;
-	n.param<double>("frontier_width", frontier_width, 0.0);
-	loc.setFrontierWidth(frontier_width);
-	
-	ros::Subscriber costmapHandler = n.subscribe<nav_msgs::OccupancyGrid> ("local_costmap", 1, handleCostmap);
-	pub_twist = n.advertise<geometry_msgs::Twist> ("cmd_vel", 1);
-	marker = n.advertise<visualization_msgs::Marker> ("cross_marker",50, true);
-	roadMarker = n.advertise<visualization_msgs::Marker> ("cross_line_marker", 50, true);
-	fakeLaserMarker = n.advertise<sensor_msgs::LaserScan> ("fake_laser_scan", 50, true);
-	
-	NavServer nav_server(n, "navigating_jockey", boost::bind(&executeNavigate, _1, &nav_server), false);
-	nav_server.start();
+  /* Minimal frontier width */
+  if (!n.hasParam("frontier_width"))
+  {
+    ROS_ERROR("Parameter frontier_width not set, exiting.");
+    return 1;
+  }
+  double frontier_width;
+  n.param<double>("frontier_width", frontier_width, 0.0);
+  loc.setFrontierWidth(frontier_width);
 
-	ROS_INFO("navigating jockey with cost map started");
-	ros::spin();
+  ros::Subscriber costmapHandler = n.subscribe<nav_msgs::OccupancyGrid> ("local_costmap", 1, handleCostmap);
+  pub_twist = n.advertise<geometry_msgs::Twist> ("cmd_vel", 1);
+  marker = n.advertise<visualization_msgs::Marker> ("cross_marker",50, true);
+  roadMarker = n.advertise<visualization_msgs::Marker> ("cross_line_marker", 50, true);
+  fakeLaserMarker = n.advertise<sensor_msgs::LaserScan> ("fake_laser_scan", 50, true);
 
-	return 0;
+  NavServer nav_server(n, "navigating_jockey", boost::bind(&executeNavigate, _1, &nav_server), false);
+  nav_server.start();
+
+  ROS_INFO("navigating jockey with cost map started");
+  ros::spin();
+
+  return 0;
 }
 
